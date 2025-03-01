fb60027dabd741f9bab0244686610840820e621e
HellLover <68247946+HellLover@users.noreply.github.com>
2025-03-01 14:52:59 +0400

    Update Navbar.tsx

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -3,7 +3,7 @@ const Navbar = () => {
         <nav className="sticky top-0 bg-gray-900 p-4 z-50">
             <div className="max-w-2xl mx-auto flex justify-between items-center border-1 border-gray-800 bg-gray-800 rounded-full p-4 shadow-sm shadow-gray-900">
                 <div className='ml-4 text-white font-bold text-xl pointer-events-none'>
-                    helllover<span className='text-blue-600'>.dev</span>
+                    kuyumjyan<span className='text-blue-600'>dev</span>
                 </div>
                 <a href="#contact" className="text-gray-300 font-bold hover:text-blue-600 duration-300 text-md mr-6 border-1 rounded-full p-[4px_8px]">Contact</a>
             </div>
