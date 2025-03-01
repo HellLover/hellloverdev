6e0c8a6b8c7c0f499b1c5d165b6ca819983323e6
HellLover <68247946+HellLover@users.noreply.github.com>
2025-03-01 14:52:24 +0400

    Update Home.tsx

--- src/components/sections/Home.tsx
+++ src/components/sections/Home.tsx
@@ -7,7 +7,7 @@ const Home = () => {
     <Hero>
         <RevealOnScroll>
             <h1 id="home" className="text-4xl sm:text-5xl lg:text-6xl font-extrabold mb-4">
-                👋 Hi, I'm <span className="text-blue-600">HellLover</span>
+                👋 Hi, I'm <span className="text-blue-600">Samvel</span>
             </h1>
             <p className="max-w-3xl text-lg sm:text-xl lg:text-2xl mb-8 ">
                 Mid-level full-stack web developer specializing in Next.js and TypeScript. Passionate about building scalable web applications, optimizing user experiences, and developing innovative solutions
